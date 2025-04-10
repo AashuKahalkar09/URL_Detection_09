bd02b58d8cd4eacffac60d936d7f90b363f3f468
AashuKahalkar09 <168652520+AashuKahalkar09@users.noreply.github.com>
2025-04-10 21:09:53 +0530

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with st.expander("PROJECT DETAILS"):
              'I created my own data set and defined features, some from the literature and some based on manual analysis. '
              'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
     st.write('The source code and data sets are available in the below Github link:')
-    st.write('https://github.com/AashuKahalkar09/URL_Detection_09')
+    st.write('https://github.com/AashuKahalkar09/URL_Detection_09 ')
 
     st.subheader('Data set')
     st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
